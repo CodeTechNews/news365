5378d87fafc64557c10cef40a4d428d4f4746df7
Jose Orlando Ramirez <yeoshen@hotmail.com>
2020-08-30 00:49:25 -0500

    mejoras pagina de detalle noticias

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,29 @@
     <title>News App</title>
     <link rel="stylesheet" href="/css/main.css">
 </head>
+<style>
+#myBtn {
+  display: none;
+  position: fixed;
+  bottom: 50px;
+  right: 30px;
+  z-index: 99;
+  font-size: 18px;
+  border: none;
+  outline: none;
+  background-color: royalblue;
+  color: white;
+  cursor: pointer;
+  padding: 15px;
+  border-radius: 4px;
+}
+
+#myBtn:hover {
+  background-color: #555;
+}
+</style>
 <body class="font-sans bg-gray-900 text-white"> 
+<button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
 <nav class="border-b border-gray-800">
         <div class="container mx-auto flex flex-col md:flex-row items-center justify-between px-4 py-6">
             <ul class="flex flex-col md:flex-row items-center">
@@ -39,5 +61,26 @@
         </div>
     </nav>
     @yield('content')
+    <script>
+    //Get the button
+    var mybutton = document.getElementById("myBtn");
+
+    // When the user scrolls down 20px from the top of the document, show the button
+    window.onscroll = function() {scrollFunction()};
+
+    function scrollFunction() {
+    if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+    }
+
+    // When the user clicks on the button, scroll to the top of the document
+    function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+    }
+    </script>
 </body>
 </html>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,6 +2,17 @@
 
 
 @section('content')
+    <style> 
+        #footer { 
+            position: fixed; 
+            padding: 10px 10px 0px 10px; 
+            bottom: 0; 
+            width: 100%; 
+            /* Height of the footer*/  
+            height: 40px; 
+            background: grey; 
+        } 
+    </style> 
     <div class="new-info border-b border-gray-800">
         <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
             <img src="{{$noticia['imagen']}}" alt="" class=" sm:h-full md:h-32 lg:h-64  object-cover" >
@@ -15,5 +26,8 @@
                 </p>
             </div>
         </div>
+        <div id="footer">
+            <marquee scrollamount="10">Estamos en período de evaluación para el lanzamiento oficial de su canal web preferido "noticias 365"</marquee> 
+        </div> 
     </div>
 @endsection
