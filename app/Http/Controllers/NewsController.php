588cf013eeafa5057f17d184be802431812bb349
Jose Orlando Ramirez <yeoshen@hotmail.com>
2020-09-05 20:19:25 -0500

    mejoras scraping noticias24

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Goutte\Client;
+use PhpParser\Node\Stmt\TryCatch;
 use Stichoza\GoogleTranslate\GoogleTranslate;
 
 class NewsController extends Controller
@@ -120,18 +121,24 @@ class NewsController extends Controller
                 die;
                 break;
             case "pol":
+                // $this->BuscarNegocios("hamburguesas playa del carmen");
+                // die;
                 return $news=[];
                 break;
             case "fin":
-                return $news=[];
+                $url = "https://finviz.com/news.ashx";
+                $filter = '.content > div > div > div > table > tr > td > table > tr > td > a';
+                ///html/body/div[2]/div/div/div/table/tbody/tr[2]/td[1]/table/tbody/tr[1]
+                $language = "en";
+                return $this->buscar_noticias($url, $filter, $language );
                 break;
             default:
                 $url = "https://www.noticias24mundo.com/";
-                $filter = ".container-fluid > div > div > div > div > div > article > div";
+                $filter = ".container-fluid > div > div > div > div > div > article";
         }
 
         $crawler = $this->scraping_url($url);
-        //dump($crawler->filter($filter));
+        //dump($crawler->filter($filter)->html());die;
 
         //$this->LeerRSS('https://www.theverge.com/rss/index.xml');
         //die;
@@ -152,27 +159,93 @@ class NewsController extends Controller
             //     echo '<br>';
             // } 
             
-            
-            if ($i>0) {
-                $imagen = $node->filter('amp-img')->attr('src');
-                $texto = $node->text();
-                $link =  $node->filter('a')->attr('href');
+            //echo $node->html();die;
+            $this->imagen = "/img/news365.png";
+            $this->texto = "NO HAY NOTICIA";
+            $this->link = "/";
+            try {
+                // echo $node->filter('a > amp-img')->attr('alt') . '<br>';
+                // echo $node->filter('a > amp-img')->attr('src') . '<br>';
+                // echo $node->filter('a')->attr('href') . "<br><br>";//code...
+                $this->texto = $node->filter('a > amp-img')->attr('alt');
+                $this->link = $node->filter('a')->attr('href');
+                $this->imagen =$node->filter('a > amp-img')->attr('src');
+
                 $this->news[] = [
                     'id'    => $i,
-                    'image' => $imagen,
-                    'text' => $texto, //substr($texto,0,100) . '...',
-                    'href'=> $link,
+                    'image' => $this->imagen,
+                    'text' => $this->texto, //substr($texto,0,100) . '...',
+                    'href'=> $this->link,
                     'lang' => $language
                 ];
+            
+
+            } catch (\Throwable $th) {
+                //throw $th;
             }
+            
+            // if ($i>0) {
+                
+            //     //$imagen = $node->filter('amp-img')->attr('src');
+            //     $imagen = "/img/news385.png";
+            //     $texto = $node->text();
+            //     dump($texto);die;
+            //     $link =  $node->filter('a')->attr('href');
+            // }
+            
+            //$image_content = file_get_contents($imagen);
+            //file_put_contents('images/' . $i . '.jpg', $image_content);
+            $i++;
+        });
+        //die;
+        //dump($this->news);die;
+        
+        return $this->news;
+    }
+
+    private function buscar_noticias($url, $filter, $language ){
+
+        $crawler = $this->scraping_url($url);
+        $i = 0;
+        //echo $filter; 
+        //dump($crawler->filter($filter)->html());die;
+
+        $crawler->filter($filter)->each(function ($node) use (&$i, &$language) {
+        
+            
+            $texto = $node->text();
+            // if ($language == "en") {
+            //     $texto = GoogleTranslate::trans($texto, 'es');
+            // }
+            $link = $node->filter('a')->attr('href');
+            $imagen = "/img/news365.png";//"https://finviz.com/gfx/news/logo/5.png";
+            //echo $texto . "<br>";
+            //echo $link . "<br>";
+            
+            
+
+            // if ($i == 3){
+            //      die;
+            // }
+            // //$texto = $node->text();
+            //$link =  $node->filter('a')->attr('href');
+            $this->news[] = [
+                'id'    => $i,
+                'image' => $imagen,
+                'text' => $texto, //substr($texto,0,100) . '...',
+                'href'=> $link,
+                'lang' => $language
+            ];
+        
             
             //$image_content = file_get_contents($imagen);
             //file_put_contents('images/' . $i . '.jpg', $image_content);
             $i++;
         });
         //dump($this->news);die;
-        
+        //die;
         return $this->news;
+
     }
 
     private function scraping_url($url){
@@ -217,6 +290,7 @@ class NewsController extends Controller
         }
         catch  (\Exception $e){
             $traducido = $texto;
+            //dump($e->getMessage());
         }
         //echo $traducido;die;
         //dump($traducido);die;
@@ -244,6 +318,87 @@ class NewsController extends Controller
         return $news;
         //dump($response);
     }
+
+    private function QueryCampo ($json, $campo, $defecto) {
+        $resultado = $defecto;
+        try {
+            $resultado = $json['result'][$campo]; //code...
+        } catch (\Throwable $th) {
+            //throw $th;
+        }
+        return $resultado;
+    }
+
+    private function BuscarNegocios($query){
+        $url = "https://maps.googleapis.com/maps/api/place/textsearch/json?key=AIzaSyBTy-SCuolM7qtHuQ39hiIHamckM6HXUQU&query=" . $query;
+        
+        $negocios = Http::get($url)->json();
+        //dump($negocios['results']);die;
+        foreach($negocios['results'] as $negocio){
+            
+            try {
+                $website = "No tiene";
+                $telefono = "xxx";
+                $reclamar = False;
+                $num_califica = 0;
+                $name = $negocio['name'];
+                $id = $negocio['place_id'];
+                //dump($name);
+                $url_details = "https://maps.googleapis.com/maps/api/place/details/json?place_id=" . $id . "&key=AIzaSyBTy-SCuolM7qtHuQ39hiIHamckM6HXUQU";
+                $detalle = Http::get($url_details)->json();
+                $url = $detalle['result']['url'];
+                $direccion = $detalle['result']['formatted_address'];
+                //$num_califica = $detalle['result']['user_ratings_total'];
+
+                $num_califica = $this->QueryCampo($detalle, 'user_ratings_total', 0);
+                $telefono = $this->QueryCampo($detalle,'international_phone_number', 'xxx-xxxxxxx');
+                $num_califica = $this->QueryCampo($detalle,'user_ratings_total', 0);
+                $website = $this->QueryCampo($detalle,'website', 'No tiene');
+                
+                
+                //dump($detalle['result']);die;
+                // $pagina = file_get_contents($url);
+                // if (strpos($pagina, 'Reclamar esta empresa') != 0){
+                //     $reclamar = True;
+                // }
+
+                //$crawler = $this->scraping_url($url);
+                //echo "uno"; dump($crawler);
+                // $url= "https://www.google.com/maps/place/DentalSPA,+Dentistas+en+Playa+del+Carmen,+10+avenida+nte+y+calle+1ra+Sur+Centro,+Calica,+77710+Playa+del+Carmen,+Q.R./@20.622406,-87.076704,17z/data=!4m2!3m1!1s0x8f4e433bfcbd3e31:0x8b20857403223375";
+                // $crawler = $this->scraping_url($url);
+                // echo "dos"; dump($crawler);die;
+                //$extracto = $crawler->filter('html > body > jsl')->html();
+
+                // dump($extracto);die;
+                // // $texto = "Empieza aqui:";
+                
+                // $crawler->filter('.app > div')->each(function ($node) use (&$texto) {
+                //     $texto .= $node->text();
+                //     dump($node);
+                // });
+                // die;
+
+
+            }
+            catch(\Exception $e) {
+
+                echo $e->getMessage(); 
+                
+            }
+
+            $empresas[] = [
+                'nombre'    => $name,
+                'direccion' => $direccion,
+                'telefono'=> $telefono,
+                'website'=> $website,
+                'id' => $id,
+                'Por_reclamar' => $reclamar, //substr($texto,0,100) . '...',
+                'url'=> $url,
+                'revisiones'=> $num_califica
+            ];          
+        }
+        dump($empresas);die;
+    } 
 
     private function LeerRSS($feedURL, $language){
 
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -9,15 +9,17 @@
             bottom: 0; 
             width: 100%; 
             /* Height of the footer*/  
-            height: 40px; 
-            background: grey; 
+            opacity: 0.9;
+            height: 80px; 
+            background: #797979; 
         } 
+        a { color: #1E8BE6; }
     </style> 
     <div class="new-info border-b border-gray-800">
         <div class="container mx-auto px-4 py-16 flex flex-col md:flex-row">
             <img src="{{$noticia['imagen']}}" alt="" class=" sm:h-full md:h-32 lg:h-64  object-cover" >
             <div class="md:ml-24 lg:ml-10">
-                <h2 class="text-4xl font-semibold">{{ $noticia['titulo'] }}</h2>
+                <h2 class="text-3xl font-semibold">{{ $noticia['titulo'] }}</h2>
                 <p class="text-gray-500 mt-8 font-mono font-hairline text-xl">
                    
                     
