33051980f03dccf3ea32fdd6057c0f77e0742ba4
Jose Orlando Ramirez <yeoshen@hotmail.com>
2020-09-13 18:07:35 -0500

    se agrego corrección lupa search

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -58,7 +58,7 @@
                     <div class="relative">
                         <input type="text" class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 mt-4 focus:outline-none focus:shadow-outline" placeholder="Search">
                         <div class="absolute top-0">
-                            <svg class="fill-current w-4 text-gray-500 md:mt-2 mt-6 ml-2" viewbox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0016 9.5 6.5 6.5 0 109.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
+                            <svg class="fill-current w-4 text-gray-500 mt-6 ml-2" viewbox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0016 9.5 6.5 6.5 0 109.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
                         </div>
                     </div>
                 </div>
