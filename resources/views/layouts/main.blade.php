45b191c86e262398fb9f5c0e796e97e0cd48659d
Jose Orlando Ramirez <yeoshen@hotmail.com>
2020-09-13 17:36:57 -0500

    se agrego nuevo logo mejora justificacion menu para tabletas

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -31,32 +31,32 @@
 <body class="font-sans bg-gray-900 text-white"> 
 <button onclick="topFunction()" id="myBtn" title="Go to top">Top</button>
 <nav class="border-b border-gray-800">
-        <div class="container mx-auto flex flex-col md:flex-row items-center justify-between px-4 py-6">
-            <ul class="flex flex-col md:flex-row items-center">
+        <div class="container mx-auto flex flex-col lg:flex-row items-center justify-between px-4 py-6">
+            <ul class="flex flex-col md:flex-col lg:flex-row items-center ">
                 <li>
                     <a href="/">
-                        <img src="/img/news365.png" alt="logo" class="rounded-full w-32 h-32">
+                        <img src="/img/news365.png" alt="logo" class="w-34 h-32">
                     </a>
                 </li>
-                <li class="lg:ml-16 md:ml-6 mt-2 md:mt-0">
+                <li class="lg:ml-16 mt-3 ">
                     <a href="/?news=inter" class="hover:text-gray-300">Internacionales</a>
                 </li>
-                <li class="md:ml-6 mt-3 md:mt-0">
+                <li class="lg:ml-6 mt-3 ">
                     <a href="/?news=tech" class="hover:text-gray-300">Tecnología</a>
                 </li>
-                <li class="md:ml-6 mt-3 md:mt-0">
+                <li class="lg:ml-6 mt-3 ">
                     <a href="/?news=pol" class="hover:text-gray-300">Política</a>
                 </li>
-                <li class="md:ml-6 mt-3 md:mt-0">
+                <li class="lg:ml-6 mt-3 ">
                     <a href="/?news=fin" class="hover:text-gray-300">Finanzas</a>
                 </li>
-                <li class="md:ml-6 mt-3 md:mt-0">
+                <li class="lg:ml-6 mt-3 ">
                     <a href="/blog" class="hover:text-gray-300">Blog</a>
                 </li>
             </ul>
                 <div class="flex flex-col md:flex-row items-center">
                     <div class="relative">
-                        <input type="text" class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 md:mt-0 mt-4 focus:outline-none focus:shadow-outline" placeholder="Search">
+                        <input type="text" class="bg-gray-800 text-sm rounded-full w-64 px-4 pl-8 py-1 mt-4 focus:outline-none focus:shadow-outline" placeholder="Search">
                         <div class="absolute top-0">
                             <svg class="fill-current w-4 text-gray-500 md:mt-2 mt-6 ml-2" viewbox="0 0 24 24"><path d="M15.5 14h-.79l-.28-.27A6.471 6.471 0 0016 9.5 6.5 6.5 0 109.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"/><path d="M0 0h24v24H0z" fill="none"/></svg>
                         </div>
